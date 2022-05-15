fcac95bf4e4f9a21fd5a369fa94e6b7bd449a282
Kyrylo Hei <fuzzihead1@gmail.com>
2022-05-15 09:43:24 +0300

    add mapping object for html class_, soup_class_dict

--- parser.py
+++ parser.py
@@ -1,6 +1,12 @@
 from bs4 import BeautifulSoup
 from typing import Generator, Union, Optional, List
 
+soup_class_dict = {'job_name': '_3862j6',
+                   'job_url': '_15V35X',
+                   'salary_tag': 'jNebTl',
+                   'short_description': '_9jGwm1',
+                   'full_description_raw': '_1yTVFy'}
+
 
 def count_pages_amount():
     pass
@@ -15,13 +21,13 @@ def parse_main_page(page: str) -> Generator[dict, None, None]:
     jobs_items = soup.find_all('article', class_='FxQpvm yKsady')
 
     for job_item in jobs_items:
-        job_name = job_item.find('span', class_='_3862j6').find('span').text.strip()
-        job_url = job_item.find('div', class_='_15V35X').find('a').get('href')
-        salary_tag = job_item.find('p', class_='jNebTl')
+        job_name = job_item.find('span', class_=soup_class_dict['job_name']).find('span').text.strip()
+        job_url = job_item.find('div', class_=soup_class_dict['job_url']).find('a').get('href')
+        salary_tag = job_item.find('p', class_=soup_class_dict['salary_tag'])
         salary = None
         if salary_tag is not None:
             salary = salary_tag.text.strip()
-        short_description = job_item.find('div', class_='_9jGwm1').text.strip()
+        short_description = job_item.find('div', class_=soup_class_dict['short_description']).text.strip()
         yield create_item_main_page(job_name, job_url, salary, short_description)
 
 
@@ -43,7 +49,7 @@ def create_item_main_page():
 
 def parse_subpage(page: str, item: dict):
     soup = BeautifulSoup(page, 'lxml')
-    full_description_raw = soup.find_all('div', class_='_1yTVFy')
+    full_description_raw = soup.find_all('div', class_=soup_class_dict['full_description_raw'])
     full_description = ' '.join(full_description_raw[1].text.split())
     return subpage_update_item(item, full_description)
 
